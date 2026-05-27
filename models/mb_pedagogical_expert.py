d9c3687beda18a2b9a1c4c19599fb4d33f217cc5
Silja Keßler <silja.kessler@student.uni-tuebingen.de>
2026-05-27 10:04:53 +0200

    Added template for pedagogical expert

---
+++ models/mb_pedagogical_expert.py
@@ -0,0 +1,155 @@
+import numpy as np
+from utils.helper_functions import softmax_policy, q_learning, dynaq_planner, find_reward, model_update
+
+"""Policy of model-based pedagogical expert."""
+
+def mb_pedagogical_expert(params, env, world, rewards_info, max_steps, n_episodes_total, n_teach_episodes, rng, optimization = False, world_model='baseline', learner_function = None, n_learner = None):
+
+    # Assert that exp is either 'baseline' or 'exp2' or 'exp3'
+    assert world_model in ['baseline', 'exp2', 'exp3'], "exp must be 'baseline', 'exp2', or 'exp3'"
+
+    ## Initializations ##
+    # Start with a uniform value function
+    value = np.ones((env.n_states, env.n_actions)) 
+    # To save the value function for each episode
+    value_perepi = np.zeros((n_episodes_total, env.n_states, env.n_actions))
+
+    # Initialize transition belief
+    transition_belief = env.init_transit_mat
+    agent_episodes = 20 
+    freq = agent_episodes // 5
+    belief_perepi = np.zeros((freq+1 , env.n_states, env.n_actions, env.n_states))
+    # Add initial transition 
+    belief_perepi[0, :, :] = transition_belief
+
+    # Initialize the model. Saves the reward and transition for each state-action pair
+    model_r = np.nan*np.zeros((env.n_states, env.n_actions, 2))
+        
+    # Initialize the reward mat
+    reward_per_step = np.zeros((n_episodes_total, max_steps)) 
+    reward_sums_epi = np.zeros((n_episodes_total))
+
+    # Initialize state mat as 
+    state_mat = np.nan*np.zeros((n_episodes_total, max_steps+1), dtype=int)
+
+    # Initialize action mat as 
+    action_mat = np.nan*np.zeros((n_episodes_total, max_steps), dtype=int)
+
+    steps_to_reward = np.nan*np.zeros((n_episodes_total,))
+
+    # Initialize storage for value function per episode
+    value_steps_list_epi = []
+    value_finalepi_list = []
+
+    # TODO: Initialize dictionary of learners for n_learner
+    learner_candidates = {}
+
+    train_episodes = n_episodes_total - n_teach_episodes
+
+    ## LOOP OVER EPISODES ##
+    for episode in range(n_episodes_total):
+        
+        value_steps_list = []
+
+        # Get rewards
+        reward_placed = rewards_info[-n_episodes_total:, :][episode][:,:2]
+
+        # Get location
+        agent_location = env.initial_loc(exp = "baseline")
+        state = world[agent_location]
+        state_mat[episode, 0] = state
+
+        # Sample n_steps from Poisson distribution with mean lambda 
+        n_steps = rng.poisson(params['lambda'], size=max_steps)
+
+        # Loop over steps
+        for t in range(max_steps):
+
+            # --- Training (identical to mb_expert) ---
+            if episode < train_episodes:
+                # Chose the next action randomly
+                _, action = softmax_policy(value, state, env.n_actions, params['beta'], rng)
+            
+                # Calculate new location based on the action
+                next_agent_location, next_state = env.move_agent(action, state, agent_location, reward_placed)
+
+                # Observe reward for that action 
+                reward = find_reward(state, reward_placed)  
+                reward_per_step[episode, t] = reward 
+                reward_sums_epi[episode] += reward
+
+                # Update the value function based on the observed reward and transition
+                value = q_learning(value, state, action, next_state, reward, params['gamma'], params['alpha'])
+            
+                # Update reward model
+                model_r = model_update(model_r, state, action, reward, next_state)
+                
+                # DYNA-Q planning 
+                value = dynaq_planner(value, 
+                                    model_r, 
+                                    state, action, next_state, 
+                                    n_steps[t], params['gamma'], params['alpha'], rng)
+
+
+                # Update transition beliefs
+                kronecker_delta = np.zeros((env.n_states,))
+                if next_state == None:
+                    next_state = state
+                kronecker_delta[next_state] = 1
+                transition_belief[state, action] = transition_belief[state, action] + params['alpha_t']*(kronecker_delta - transition_belief[state, action])
+                # Normalize the transition matrix
+                transition_belief[state, action, :] = transition_belief[state, action, :] / np.sum(transition_belief[state, action, :])
+
+                value_steps_list.append(np.copy(value))          
+
+            # --- Teaching ---
+            else:
+                # TODO: Implement teaching policy. Does the expert continue to learn during the teaching episodes? If so, move this part to line 72
+                for a in range(4):
+                    for learner in range(n_learner):
+                        # Simulate learner's behavior based on potential action to demonstrate in expert state --> "state" (move as separate function to helper_functions.py)
+                        pass
+                
+                # Compute objective (e.g., average difference between value of selected action of learner and expert's optimal action) and select action that maximizes the objective (move as separate function to helper_functions.py)
+                # If we use the average difference between value of selected action of learner and expert's optimal action, we need the optimal action of the expert, which could either be sampled from the softmax or be argmax_a of the learner's state. 
+                # But note that different learner candidates might be in different states!
+                best_action = None 
+                action = teaching_objective(learner_candidates)
+
+                # Calculate new location based on the action
+                next_agent_location, next_state = env.move_agent(action, state, agent_location, reward_placed)
+
+                # Observe reward for that action 
+                reward = find_reward(state, reward_placed)  
+                reward_per_step[episode, t] = reward 
+                reward_sums_epi[episode] += reward
+
+
+            # Store action
+            action_mat[episode,t] = action
+            
+            # If positive reward is found, end episode
+            if reward > 0:
+                steps_to_reward[episode] = t + 1
+                break 
+            
+            # Update the state
+            state = next_state
+            agent_location = next_agent_location
+
+            # Save the state
+            state_mat[episode, t+1] = state
+
+
+
+        value_steps_list_epi.append(value_steps_list)
+        value_finalepi_list.append(np.copy(value))
+        # If the episode doesnt terminate, it locates a 40
+        steps_to_reward[episode] = t+1
+
+    if optimization:    
+        # Only return the reward sums per episode for the optimization procedure
+        return reward_sums_epi
+    
+    else:
+       return value, reward_sums_epi, state_mat, action_mat, steps_to_reward, transition_belief, model_r, value_perepi, belief_perepi, reward_per_step # value_finalepi_list, value_steps_list_epi
---
+++ simulations/sim_mb_pedagogical_expert.py
@@ -0,0 +1,139 @@
+import numpy as np
+import json
+import tqdm
+import os
+
+from models.mb_pedagogical_expert import mb_pedagogical_expert
+from models.mf_valueshaping import mf_valueshaping
+from utils.plot_functions import plot_performance
+from utils.world import VillageWorld
+
+"""Simulation of model-based pedagogical expert."""
+
+# Simulation parameters
+save = True
+optimization = False
+world_model = "baseline" # "baseline" "exp2" "exp3"
+n_states = 100
+n_simulations = 1 # 1000
+n_train_episodes = 100
+n_teach_episodes = 20
+n_episodes_total = n_train_episodes + n_teach_episodes 
+max_steps = 40
+n_actions = 4
+learner = "MF-VS"
+n_learner = 10
+
+# Set random seed for reproducibility
+seed = 5
+rng = np.random.default_rng(seed)
+
+## LOAD DATA ##
+# Load parameters
+print("Params pedagogical expert:")
+with open(f'saved/opti_results/mbased_pedagogical_expert.json', 'r') as json_file:
+    params = json.load(json_file)["opti_params"]
+print(params)
+
+# Load worlds and rewards
+loaded = np.load('saved/worlds.npz')
+worlds_saved = [loaded[f'arr_{i}'] for i in range(len(loaded.files))]
+
+rewards_load = np.load('saved/rewards_info.npz')
+rewards_shuffled = [rewards_load[f'arr_{i}'] for i in range(len(rewards_load.files))]
+
+# Load learner
+if learner == "MF-VS":
+    learner_function = mf_valueshaping
+
+# TODO: Is there anything else we want to store?
+
+## INITIALIZE STORAGE ARRAYS ##
+# Total reward for each episode
+rewards_result_epi_saved = np.zeros((n_simulations, n_episodes_total))
+# Save the reward for each step
+rewards_result_steps = np.zeros((n_simulations, n_episodes_total))
+# N* of steps until agent finds a reward
+steps_saved = np.zeros((n_simulations, n_episodes_total))
+# Final value per simulation
+value_saved = np.zeros((n_simulations,n_states,n_actions ))
+# States expert goes through - mainly to check results
+states_saved = np.zeros((n_simulations, n_episodes_total, max_steps +1 ))
+# Actions taken by the expert - Policy
+actions_saved = np.zeros((n_simulations, n_episodes_total, max_steps))
+# Model saved
+model_saved = np.zeros((n_simulations, n_states, n_actions, 2)) 
+# Final tm of each simulation
+tm_saved = []
+# Value per episode
+value_epi_saved = []
+# Tm every 5 episodes
+tm_epi_saved = []
+
+# Load agent function
+agent_function = mb_pedagogical_expert
+
+## SIMULATION LOOP ##
+print(f"MB pedagogical expert simulations with {world_model} world model")
+for sim in tqdm(range(n_simulations)):
+    env = VillageWorld(worlds_saved[sim], rng)
+    final_value, reward_sums_epi, state_mat, action_mat, steps_to_reward, tm_final, model_r, value_epi, tm_epi, reward_sums_steps  = agent_function(params, 
+                                                                                                                                                    env, 
+                                                                                                                                                    worlds_saved[sim], 
+                                                                                                                                                    rewards_shuffled[sim], 
+                                                                                                                                                    max_steps, 
+                                                                                                                                                    n_episodes_total, 
+                                                                                                                                                    n_teach_episodes,
+                                                                                                                                                    rng, 
+                                                                                                                                                    optimization = False,
+                                                                                                                                                    world_model = world_model,
+                                                                                                                                                    learner_function = learner_function,
+                                                                                                                                                    n_learner = n_learner)
+    
+    rewards_result_epi_saved[sim] = reward_sums_epi
+    steps_saved[sim,:] = steps_to_reward
+    actions_saved[sim,:] = action_mat
+    value_saved[sim, :, :] = final_value
+    states_saved[sim, :, :] = state_mat
+    model_saved[sim, :, :, :] = model_r
+    tm_saved.append(tm_final)
+    value_epi_saved.append(value_epi)
+    tm_epi_saved.append(tm_epi)
+
+
+## PLOT PERFORMANCE ##
+title = f"MB pedagogical expert {n_simulations} sim"
+fig1, ax1 = plot_performance(rewards_result_epi_saved, "Episodes", title , "Performance", None)
+fig2, ax2 = plot_performance(steps_saved, "Episodes", title, "Steps to reward", None)
+
+## SAVE DATA ##
+data = {"sum_rewards": rewards_result_epi_saved.tolist(), 
+        "steps_to_reward": steps_saved.tolist(), 
+        "value": value_saved.tolist(), 
+        "states_saved": states_saved.tolist(),
+        "actions_saved": actions_saved.tolist(),
+        "model_saved": model_saved.tolist()
+        }
+
+if save:
+    # Create directory if it doesn't exist
+    saving_path = f'saved/{world_model}'
+    os.makedirs(saving_path, exist_ok=True)
+    os.makedirs(f'{saving_path}/tmss', exist_ok=True)
+
+    with open(f'saved/{world_model}/mbased_pedagogical_expert_{world_model}.json', 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+    np.savez_compressed(f'saved/{world_model}/tmss/mbased_pedagogical_expert_tm.npz', *tm_saved)
+    np.savez_compressed(f'saved/{world_model}/mbased_pedagogical_expert_value_epi.npz', *value_epi_saved)
+    np.savez_compressed(f'saved/{world_model}/tmss/mbased_pedagogical_expert_tm_epi.npz', *tm_epi_saved)
+    print(f"MB pedagogical expert data saved")
+
+    # Save figures
+    folder_path = os.path.join('saved', 'figures', str(world_model))
+    # Create the folder if it doesn't exist
+    os.makedirs(folder_path, exist_ok=True)
+
+    fig1.savefig(os.path.join(folder_path, f'mb_pedagogical_expert_{world_model}_performance.png'), bbox_inches='tight')
+    fig2.savefig(os.path.join(folder_path, f'mb_pedagogical_expert_{world_model}_steps.png'), bbox_inches='tight')
+    print(f"Figures saved for MB pedagogical expert")
